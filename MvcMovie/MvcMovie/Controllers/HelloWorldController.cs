cd607a68f998917cf8cd5c0fda777ed620562e19
FredericoSilva015 <fredsilva015@gmail.com>
2016-01-13 19:56:36 +0000

    code

---
+++ MvcMovie/MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,24 @@
+﻿
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+ 
+        // 
+        // GET: /HelloWorld/Welcome/ 
+
+        public ActionResult Welcome(string name, int ID = 1)
+        {
+            ViewBag.Message = name;
+            ViewBag.Id = ID;
+            return View();
+        }
+    }
+}
---
+++ Teste/Teste/Models/Table.cs
@@ -0,0 +1,15 @@
+﻿using System.Data.Entity;
+
+namespace Teste.Models
+{
+    public class Table
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class TestContext : DbContext
+    {
+        public IDbSet<Table> Tables { get; set; }
+    }
+}
